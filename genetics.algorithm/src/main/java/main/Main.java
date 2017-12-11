473456754f0a9db5f9828e045083b10a9d746c59
Hardik Jain <jain.h@husky.neu.edu>
2017-12-11 12:44:05 -0500

    Project replaced

---
+++ genetics.algorithm/src/main/java/main/Main.java
@@ -0,0 +1,37 @@
+package main;
+
+import org.apache.log4j.Logger;
+
+import constants.Constants;
+import flight.Flight;
+import population.Population;
+
+public class Main {
+
+	final static Logger logger = Logger.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		Population pop = new Population();
+		pop.generatePopulation();
+		logger.info("***********************Generation : 0**************************");
+		logger.info("Fittest : " + pop.getFittest().getFitness());
+		int tries = 1;
+		while (tries <= Constants.MIN_GENERATIONS || pop.getFittest().getFitness() < Constants.FITNESS_THRESHOLD) {
+			logger.info("***********************Generation : " + tries + "**************************");
+			pop.evolve();
+			Flight fittest = pop.getFittest();
+			logger.info("Fittest : " + fittest.getFitness());
+			logger.info("Fare : $" + fittest.getPrice());
+			logger.info("Duration : " + fittest.getTime() + " hours");
+			tries++;
+		}
+		Flight fittest = pop.getFittest();
+		logger.info("****************************************************");
+		logger.info("Number of generations : " + (tries - 1));
+		logger.info("Optimal flight fitness : " + fittest.getFitness());
+		logger.info("Fare : $" + fittest.getPrice());
+		logger.info("Duration : " + fittest.getTime() + " hours");
+
+	}
+
+}
